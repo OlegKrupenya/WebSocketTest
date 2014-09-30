2e7a0b7f90ae77bf3effbedbace07197ba7f82f4
unknown <taipan90@rambler.ru>
2014-09-30 19:46:44 +0300

    initial commit

---
+++ src/main/java/server/EchoServer.java
@@ -0,0 +1,55 @@
+package server;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by Taipan on 28.09.2014.
+ */
+@ServerEndpoint("/echo")
+public class EchoServer {
+    /**
+     * @OnOpen allows us to intercept the creation of a new session.
+     * The session class allows us to send data to the user.
+     * In the method onOpen, we'll let the user know that the handshake was
+     * successful.
+     */
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println(session.getId() + " has opened a connection");
+        try {
+            session.getBasicRemote().sendText("Connection Established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * When a user sends a message to the server, this method will intercept the message
+     * and allow us to react to it. For now the message is read as a String.
+     */
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println("Message from " + session.getId() + ": " + message);
+        try {
+            session.getBasicRemote().sendText(message + " from the server");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * The user closes the connection.
+     *
+     * Note: you can't send messages to the client from this method
+     */
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("Session " +session.getId()+" has ended");
+    }
+}
